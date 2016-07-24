c8c08f2e33553dbc47d129889a9d33f12c2bcd79
Didifer123 <sal15487@uvg.edu.gt>
2016-07-24 09:09:36 -0600

    iPila.java
    Esta es la interfaz de la pila

---
+++ iPila.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package postfix;
+
+/**
+ *
+ * @author Didier
+ */
+public interface iPila {
+    public void operar();
+    public int resultado();
+    public String toString();
+}
